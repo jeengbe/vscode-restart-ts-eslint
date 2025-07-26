23fdaa4792b0cc83cfb1b8daa250d9e04cf7aee3
Jesper Engberg <jeengbe@gmail.com>
2025-07-26 13:25:24 +0200

    fix: only display one button

--- src/extension.ts
+++ src/extension.ts
@@ -1,79 +1,53 @@
-"use strict";
-import * as vscode from "vscode";
+'use strict';
+import * as vscode from 'vscode';
 
-let restartTsEslintTs: vscode.StatusBarItem;
-let restartTsEslintEslint: vscode.StatusBarItem;
 let restartTsEslintBoth: vscode.StatusBarItem;
 
-const TYPESCRIPT_EXTENSION_ID = "vscode.typescript-language-features";
-const ESLINT_EXTENSION_ID = "dbaeumer.vscode-eslint";
+const TYPESCRIPT_EXTENSION_ID = 'vscode.typescript-language-features';
+const ESLINT_EXTENSION_ID = 'dbaeumer.vscode-eslint';
 
-const RESTART_TS_SERVER_LABEL = "$(debug-restart) Restart TS";
-const RESTART_ESLINT_SERVER_LABEL = "$(debug-restart) Restart ESLint";
-const RESTART_BOTH_LABEL = "$(debug-restart) Restart Both";
-const THIS_EXT_NAME = "restart-ts-eslint-server";
-const THIS_EXT_ID = `acoreyj.${THIS_EXT_NAME}`;
+const RESTART_BOTH_LABEL = '$(debug-restart) Restart TS And ESLint Server';
+const THIS_EXT_NAME = 'restart-ts-eslint-server';
+const THIS_EXT_ID = `jeengbe.${THIS_EXT_NAME}`;
 const SUPPORTED_LANGUAGES = [
-  "javascript",
-  "javascriptreact",
-  "typescript",
-  "typescriptreact",
-  "svelte"
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'svelte',
 ];
 
 export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      `${THIS_EXT_NAME}.softRestartTsServer`,
-      softRestartTsServer
-    ),
-    vscode.commands.registerCommand(
-      `${THIS_EXT_NAME}.softRestartEslintServer`,
-      softRestartEslintServer
-    ),
-    vscode.commands.registerCommand(
       `${THIS_EXT_NAME}.softRestartBoth`,
-      softRestartBoth
-    )
+      softRestartBoth,
+    ),
   );
-
-  restartTsEslintTs = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
-    1
-  );
-  restartTsEslintTs.command = `${THIS_EXT_NAME}.softRestartTsServer`;
-  restartTsEslintTs.text = RESTART_TS_SERVER_LABEL;
-
-  restartTsEslintEslint = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
-    1
-  );
-  restartTsEslintEslint.command = `${THIS_EXT_NAME}.softRestartEslintServer`;
-  restartTsEslintEslint.text = RESTART_ESLINT_SERVER_LABEL;
 
   restartTsEslintBoth = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
-    0
+    0,
   );
   restartTsEslintBoth.command = `${THIS_EXT_NAME}.softRestartBoth`;
   restartTsEslintBoth.text = RESTART_BOTH_LABEL;
 
   const restartBothCommandPalette = vscode.commands.registerCommand(
     `${THIS_EXT_NAME}.softRestartBothCommand`,
-    softRestartBoth
+    softRestartBoth,
   );
 
   context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItemVisibility)
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItemVisibility),
   );
   context.subscriptions.push(
-    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItemVisibility)
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItemVisibility),
   );
   context.subscriptions.push(
-    vscode.workspace.onDidCloseTextDocument(updateStatusBarItemVisibility)
+    vscode.workspace.onDidCloseTextDocument(updateStatusBarItemVisibility),
   );
   context.subscriptions.push(
-    vscode.workspace.onDidOpenTextDocument(updateStatusBarItemVisibility)
+    vscode.workspace.onDidOpenTextDocument(updateStatusBarItemVisibility),
   );
   context.subscriptions.push(restartBothCommandPalette);
 
@@ -82,29 +56,25 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 function softRestartEslintServer() {
-  const eslintExtension = vscode.extensions.getExtension(ESLINT_EXTENSION_ID);
-  if (!eslintExtension || eslintExtension.isActive === false) {
+  if (!vscode.extensions.getExtension(ESLINT_EXTENSION_ID)?.isActive) {
     vscode.window.showErrorMessage(
-      "ESLint extension is not active or not running."
+      'ESLint extension is not active or not running.',
     );
     return;
   }
 
-  return vscode.commands.executeCommand("eslint.restart");
+  return vscode.commands.executeCommand('eslint.restart');
 }
 
 async function softRestartTsServer() {
-  const typeScriptExtension = vscode.extensions.getExtension(
-    TYPESCRIPT_EXTENSION_ID
-  );
-  if (!typeScriptExtension || typeScriptExtension.isActive === false) {
+  if (!vscode.extensions.getExtension(TYPESCRIPT_EXTENSION_ID)?.isActive) {
     vscode.window.showErrorMessage(
-      "TypeScript extension is not active or not running."
+      'TypeScript extension is not active or not running.',
     );
     return;
   }
 
-  await vscode.commands.executeCommand("typescript.restartTsServer");
+  await vscode.commands.executeCommand('typescript.restartTsServer');
 }
 
 async function softRestartBoth() {
@@ -116,24 +86,17 @@ function updateStatusBarItemVisibility(): void {
   const { activeTextEditor } = vscode.window;
 
   if (
-    !activeTextEditor ||
-    !activeTextEditor.document ||
-    SUPPORTED_LANGUAGES.indexOf(activeTextEditor.document.languageId) === -1
+    !activeTextEditor?.document ||
+    !SUPPORTED_LANGUAGES.includes(activeTextEditor.document.languageId)
   ) {
-    restartTsEslintTs.hide();
-    restartTsEslintEslint.hide();
     restartTsEslintBoth.hide();
   } else {
-    restartTsEslintTs.show();
-    const eslintExtension = vscode.extensions.getExtension(ESLINT_EXTENSION_ID);
-    if (eslintExtension && eslintExtension.isActive  !== false) {
-     restartTsEslintEslint.show();
-     restartTsEslintBoth.show();
+    if (vscode.extensions.getExtension(ESLINT_EXTENSION_ID)?.isActive) {
+      restartTsEslintBoth.show();
     }
   }
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
   console.log(`Extension ${THIS_EXT_ID} is now inactive!`);
 }
